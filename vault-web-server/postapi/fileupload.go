c2d84c8fb7a79ffde595c4be862760b12c02cf6b
Dylan Hobbs <dylan.hobbs@vouched.id>
2023-06-27 17:25:46 -0500

    changes

--- vault-web-server/postapi/fileupload.go
+++ vault-web-server/postapi/fileupload.go
@@ -22,32 +22,21 @@ const MAX_FILE_SIZE int64 = 25 << 20         // 3 MB
 const MAX_TOTAL_UPLOAD_SIZE int64 = 50 << 20 // 3 MB
 
 func (ctx *HandlerContext) UploadHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Limit the request body size
 	r.Body = http.MaxBytesReader(w, r.Body, MAX_TOTAL_UPLOAD_SIZE)
 
-	err := r.ParseMultipartForm(MAX_TOTAL_UPLOAD_SIZE) // Maximum upload of 3 MB
+	err := r.ParseMultipartForm(MAX_TOTAL_UPLOAD_SIZE)
 	if err != nil {
-		if err == http.ErrMissingBoundary || err == http.ErrNotMultipart || err == http.ErrNotSupported {
-			log.Println("[UploadHandler ERR] Error parsing multipart form:", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		log.Println("[UploadHandler ERR] Request body size exceeds the limit:", err)
-		http.Error(w, "Request body size exceeds the limit", http.StatusRequestEntityTooLarge)
+		log.Println("[UploadHandler ERR] Error parsing multipart form:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	files := r.MultipartForm.File["files"]
-	uuid := r.FormValue("uuid") // Get the UUID from the form data
+	uuid := r.FormValue("uuid")
 	userProvidedOpenApiKey := r.FormValue("apikey")
-
-	log.Println("[UploadHandler] UUID=", uuid)
 
 	clientToUse := ctx.openAIClient
 	if userProvidedOpenApiKey != "" {
-		log.Println("[UploadHandler] Using provided custom API key:", userProvidedOpenApiKey)
 		clientToUse = openai.NewClient(userProvidedOpenApiKey)
 	}
 
@@ -58,62 +47,102 @@ func (ctx *HandlerContext) UploadHandler(w http.ResponseWriter, r *http.Request)
 
 	for _, file := range files {
 		fileName := file.Filename
-
 		if file.Size > MAX_FILE_SIZE {
 			errMsg := fmt.Sprintf("File size exceeds the %d bytes limit", MAX_FILE_SIZE)
-			log.Println("[UploadHandler ERR]", errMsg, fileName)
 			responseData.NumFilesFailed++
 			responseData.FailedFileNames[fileName] = errMsg
 			continue
 		}
 
-		// Read the file in memory
 		f, err := file.Open()
 		if err != nil {
 			errMsg := "Error opening file"
-			log.Println("[UploadHandler ERR]", errMsg, err)
 			responseData.NumFilesFailed++
 			responseData.FailedFileNames[fileName] = errMsg
 			continue
 		}
 		defer f.Close()
 
-		// Get the file name, MIME type, and first 32 characters of the contents
 		fileType := file.Header.Get("Content-Type")
 		fileContent := ""
 		filePreview := ""
 
-		// Check if the file is a PDF
 		if fileType == "application/pdf" {
 			fileContent, err = chunk.ExtractTextFromPDF(f, file.Size)
 			if err != nil {
 				errMsg := "Error extracting text from PDF"
-				log.Println("[UploadHandler ERR]", errMsg, err)
 				responseData.NumFilesFailed++
 				responseData.FailedFileNames[fileName] = errMsg
 				continue
 			}
-		} else {
+		} else if fileType == "text/plain" || fileType == "text/html" || fileType == "text/css" || fileType == "application/javascript" || fileType == "application/json" || fileType == "text/typescript" {
 			fileContent, err = chunk.GetTextFromFile(f)
 			if err != nil {
 				errMsg := "Error reading file"
-				log.Println("[UploadHandler ERR]", errMsg, err)
 				responseData.NumFilesFailed++
 				responseData.FailedFileNames[fileName] = errMsg
 				continue
 			}
+		} else if fileType == "application/zip" {
+			fileContents, fileNames, err := chunk.ExtractFilesFromZip(f)
+			if err != nil {
+				errMsg := "Error extracting files from zip"
+				responseData.NumFilesFailed++
+				responseData.FailedFileNames[fileName] = errMsg
+				continue
+			}
+
+			for i, content := range fileContents {
+				fileContent = content
+				fileName = fileNames[i]
+			
+				if len(fileContent) > 32 {
+					filePreview = fileContent[:32]
+				}
+
+				chunks, err := chunk.CreateChunks(fileContent, fileName)
+				if err != nil {
+					errMsg := "Error chunking file"
+					responseData.NumFilesFailed++
+					responseData.FailedFileNames[fileName] = errMsg
+					continue
+				}
+
+				embeddings, err := getEmbeddings(clientToUse, chunks, 100, openai.AdaEmbeddingV2)
+				if err != nil {
+					errMsg := fmt.Sprintf("Error getting embeddings: %v", err)
+					responseData.NumFilesFailed++
+					responseData.FailedFileNames[fileName] = errMsg
+					continue
+				}
+
+				err = ctx.vectorDB.UpsertEmbeddings(embeddings, chunks, uuid)
+				if err != nil {
+					errMsg := fmt.Sprintf("Error upserting embeddings to vector DB: %v", err)
+					responseData.NumFilesFailed++
+					responseData.FailedFileNames[fileName] = errMsg
+					continue
+				}
+
+				responseData.NumFilesSucceeded++
+				responseData.SuccessfulFileNames = append(responseData.SuccessfulFileNames, fileName)
+			}
+
+			continue
+		} else {
+			errMsg := "File type not supported"
+			responseData.NumFilesFailed++
+			responseData.FailedFileNames[fileName] = errMsg
+			continue
 		}
 
 		if len(fileContent) > 32 {
 			filePreview = fileContent[:32]
 		}
-		log.Printf("File Name: %s, File Type: %s, File Content (first 32 characters): %s\n", fileName, fileType, filePreview)
 
-		// Process the fileBytes into embeddings and store in vector DB here
 		chunks, err := chunk.CreateChunks(fileContent, fileName)
 		if err != nil {
 			errMsg := "Error chunking file"
-			log.Println("[UploadHandler ERR]", errMsg, err)
 			responseData.NumFilesFailed++
 			responseData.FailedFileNames[fileName] = errMsg
 			continue
@@ -122,25 +151,18 @@ func (ctx *HandlerContext) UploadHandler(w http.ResponseWriter, r *http.Request)
 		embeddings, err := getEmbeddings(clientToUse, chunks, 100, openai.AdaEmbeddingV2)
 		if err != nil {
 			errMsg := fmt.Sprintf("Error getting embeddings: %v", err)
-			log.Println("[UploadHandler ERR]", errMsg)
 			responseData.NumFilesFailed++
 			responseData.FailedFileNames[fileName] = errMsg
 			continue
 		}
-		fmt.Printf("Total chunks: %d\n", len(chunks))
-		fmt.Printf("Total embeddings: %d\n", len(embeddings))
-		fmt.Printf("Embeddings length: %d\n", len(embeddings[0]))
 
 		err = ctx.vectorDB.UpsertEmbeddings(embeddings, chunks, uuid)
 		if err != nil {
 			errMsg := fmt.Sprintf("Error upserting embeddings to vector DB: %v", err)
-			log.Println("[UploadHandler ERR]", errMsg)
 			responseData.NumFilesFailed++
 			responseData.FailedFileNames[fileName] = errMsg
 			continue
 		}
-
-		log.Println("Successfully added vector DB embeddings!")
 
 		responseData.NumFilesSucceeded++
 		responseData.SuccessfulFileNames = append(responseData.SuccessfulFileNames, fileName)
@@ -156,7 +178,6 @@ func (ctx *HandlerContext) UploadHandler(w http.ResponseWriter, r *http.Request)
 	w.Header().Set("Content-Type", "application/json")
 	jsonResponse, err := json.Marshal(responseData)
 	if err != nil {
-		log.Println("[UploadHandler ERR] Error writing json response", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
